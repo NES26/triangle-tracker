cc98bd79c96b552b1c0aae8de4c52bdecaa1b006
nirnuhsimel@gmail.com <nirnuhsimel@gmail.com>
2019-03-25 09:59:45 +0300

    Add scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,6 @@ function checkinputs()
 base = parseInt(document.Triangle_Inputs.input1.value);
 height = parseInt(document.Triangle_Inputs.input2.value);
 hypotenuse = parseInt(document.Triangle_Inputs.input3.value);
-
 /*var sides = function(base,height,hypotenuse){
   return(base+height>hypotenuse && height + hypotenuse>base && hypotenuse + base>height)
 }*/
